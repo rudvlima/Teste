acb9003c260f8a2dbd6b72397f1427e78c9ccf69
rudvlima <rudvlima@mail.com>
2021-05-24 04:27:51 -0400

    Finalizado

---
+++ Algoritmos/AlgoritimosMetodos.cs
@@ -0,0 +1,26 @@
+// MÉTODOS ESTÃO NA PASTA KNEWINAPI/CONTROLER E SÃO EXECUTADOS VIA API
+
+        // public ActionResult Duplicados(int[] listaInteiros)
+        // {
+
+        //     if (listaInteiros == null || listaInteiros.Length == 0)
+        //         return Json(new { resultado = "Lista nao informada ", index = 0 });
+
+
+        //     for (int i = 0; i <= listaInteiros.Length; i++)
+        //         for (int j = i + 1; j < listaInteiros.Length; j++)
+        //             if (listaInteiros[j] == listaInteiros[i])
+        //                 return Json(new { indice = i });
+                    
+ 
+        //     return Json(new { resultado = "Sem Valor Duplicado" });
+        // }
+
+        // public ActionResult Palindromo(string palavra)
+        // {
+        //     if (string.IsNullOrEmpty(palavra))
+        //         return Json(new { resultado = "Palavra nao informada" });
+
+        //     return Json(new{ Palindromo = palavra.Equals(new string(palavra.Reverse().ToArray()))});
+        // }
+    
